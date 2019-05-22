04beb7e8e1b14cf676e899a4ca3274698d958ee1
Xerihet <50606433+Xerihet@users.noreply.github.com>
2019-05-22 16:54:48 +1000

    Update Auto_Update.bat

--- Auto_Update.bat
+++ Auto_Update.bat
@@ -1,5 +1,5 @@
 @echo on
 cd C:\ProgramData\sysmon\
-@powershell (new-object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/xerihet/sysmon-config/develop/sysmonconfig-export.xml','C:\ProgramData\sysmon\sysmonconfig-export.xml')"
+@powershell (new-object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/xerihet/sysmon-config/master/sysmonconfig-export.xml','C:\ProgramData\sysmon\sysmonconfig-export.xml')"
 sysmon64 -c sysmonconfig-export.xml
 exit
